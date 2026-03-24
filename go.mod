8b158e8e09ffb4fa285ca6e2ed42f65f8d92ae97
Sudhanshu-NITR <sudhanshu.kadam.99@gmail.com>
2026-03-24 19:38:24 +0530

    Parse Config file

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Sudhanshu-NITR/Kortex
-
-go 1.25.8
---
+++ semantic-code-agent/internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env string `yaml:"env" env:"ENV" env-required:"true" env-default:"prod"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config Path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("Cannot read file: %s", err.Error())
+	}
+
+	return &cfg
+}
